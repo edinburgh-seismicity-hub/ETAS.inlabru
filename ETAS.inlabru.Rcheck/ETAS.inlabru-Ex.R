92b6ec165efce3f26a83e61403e86957f4c896c6
Serra314 <serafini314@gmail.com>
2022-11-16 16:26:12 +0000

    temporalBinning documentation updated

--- ETAS.inlabru.Rcheck/ETAS.inlabru-Ex.R
+++ ETAS.inlabru.Rcheck/ETAS.inlabru-Ex.R
@@ -1,18 +1,6 @@
 pkgname <- "ETAS.inlabru"
 source(file.path(R.home("share"), "R", "examples-header.R"))
 options(warn = 1)
-base::assign(".ExTimings", "ETAS.inlabru-Ex.timings", pos = 'CheckExEnv')
-base::cat("name\tuser\tsystem\telapsed\n", file=base::get(".ExTimings", pos = 'CheckExEnv'))
-base::assign(".format_ptime",
-function(x) {
-  if(!is.na(x[4L])) x[1L] <- x[1L] + x[4L]
-  if(!is.na(x[5L])) x[2L] <- x[2L] + x[5L]
-  options(OutDec = '.')
-  format(x[1L:3L], digits = 7L)
-},
-pos = 'CheckExEnv')
-
-### * </HEADER>
 library('ETAS.inlabru')
 
 base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
@@ -23,7 +11,6 @@ nameEx("create.input.list.temporal.noCatalogue")
 
 flush(stderr()); flush(stdout())
 
-base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: create.input.list.temporal.noCatalogue
 ### Title: Function to create a default input file for the ETAS Hawkes
 ###   temporal model where no catalogue is specified in the input file
@@ -36,15 +23,12 @@ base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 
 
 
-base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
-base::cat("create.input.list.temporal.noCatalogue", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("generate.temporal.ETAS.synthetic")
 ### * generate.temporal.ETAS.synthetic
 
 flush(stderr()); flush(stdout())
 
-base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: generate.temporal.ETAS.synthetic
 ### Title: Generates a sythetic catalogue using the ETAS model
 ### Aliases: generate.temporal.ETAS.synthetic
@@ -63,15 +47,12 @@ generate.temporal.ETAS.synthetic( theta=data.frame(mu=0.1, K=0.089, alpha=2.29, 
 
 
 
-base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
-base::cat("generate.temporal.ETAS.synthetic", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("sample.GR.magnitudes")
 ### * sample.GR.magnitudes
 
 flush(stderr()); flush(stdout())
 
-base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: sample.GR.magnitudes
 ### Title: Return a sample of magnitudes drawn from the GR distribution
 ### Aliases: sample.GR.magnitudes
@@ -82,15 +63,12 @@ sample.GR.magnitudes(n=100, beta.p=log(10), M0=2.5)
 
 
 
-base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
-base::cat("sample.GR.magnitudes", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("sample.temporal.ETAS.generation")
 ### * sample.temporal.ETAS.generation
 
 flush(stderr()); flush(stdout())
 
-base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: sample.temporal.ETAS.generation
 ### Title: Take all previous parent events from 'Ht=data.frame[ts,
 ###   magnitudes]' and generates their daughters events using the ETAS
@@ -105,15 +83,12 @@ sample.temporal.ETAS.generation( theta=data.frame(mu=0.1, K=0.089, alpha=2.29, c
 
 
 
-base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
-base::cat("sample.temporal.ETAS.generation", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("time.grid")
 ### * time.grid
 
 flush(stderr()); flush(stdout())
 
-base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: time.grid
 ### Title: Generate a set of time bins for a specific event and return.
 ### Aliases: time.grid
@@ -130,8 +105,6 @@ time.grid(events, coef.t, delta.t, T2, displaygrid = FALSE, N.exp)
 
 
 
-base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
-base::cat("time.grid", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 ### * <FOOTER>
 ###
 cleanEx()
--- ETAS.inlabru/R/HawkesProcess.R
+++ ETAS.inlabru/R/HawkesProcess.R
@@ -11,7 +11,7 @@
 #' @examples
 Temporal.ETAS.fit <- function(input.list){
   cat('Start model fitting', '\n')
-  fit_etas <- temporal.ETAS(sample.s = input.list$catalog.bru, # data
+  fit_etas <- Temporal.ETAS(sample.s = input.list$catalog.bru, # data
                             M0 = input.list$M0, # magnitude of completeness
                             T1 = input.list$T12[1],
                             T2 = input.list$T12[2], # time domain
@@ -28,7 +28,7 @@ Temporal.ETAS.fit <- function(input.list){
 #######
 ## MN: DESCRIPTION: Function to fit an ETAS Hawkes process model to catalogue data
 #' Function to fit Hawkes process model
-#' @description function to fit a temporal ETAS model using `inlabru`. 
+#' @description function to fit a temporal ETAS model using `inlabru`.
 #' @param sample.s Observed events: `data.frame` with columns time (ts), magnitude (magnitudes), event identifier (idx.p). Column names must not be changed.
 #' @param M0 Minimum magnitude threshold, `scalar`
 #' @param T1 Start of temporal model domain, `scalar` [measure unit of sample.s$ts].
@@ -43,13 +43,13 @@ Temporal.ETAS.fit <- function(input.list){
 #' @export
 #'
 #' @examples
-temporal.ETAS <- function(sample.s, M0, T1, T2, link.functions = NULL,
+Temporal.ETAS <- function(sample.s, M0, T1, T2, link.functions = NULL,
                           coef.t., delta.t., N.max., bru.opt){
-  
+
   # Expected number of background events
   df.0 <- data.frame(counts = 0, exposures = 1, part = 'background')
   # this is the expression of log(Lambda0)
-  
+
   ## Create the grid for the XX integration
   cat('Start creating grid...', '\n')
   time.g.st <- Sys.time()
@@ -63,13 +63,13 @@ temporal.ETAS <- function(sample.s, M0, T1, T2, link.functions = NULL,
   df.j$counts <- 0
   df.j$exposures <- 1
   df.j$part = 'triggered'
-  
+
   t.names <- unique(df.j$t.ref_layer)
   time.sel <- df.j[vapply(t.names, \(bname) match(TRUE, df.j$t.ref_layer == bname), 0L), , drop = FALSE]
   Imapping <- match(df.j$t.ref_layer, t.names)
-  
+
   cat('Finished creating grid, time ', Sys.time() - time.g.st, '\n')
-  
+
   # MN: Define local function to calculate log-likelihood triggered contribution of one event to each bin.
   # MN: h is to denote it is from past events
   logLambda.h.inla <- function(th.K, th.alpha, th.c, th.p, list.input_, ncore_ = ncore){
@@ -78,7 +78,7 @@ temporal.ETAS <- function(sample.s, M0, T1, T2, link.functions = NULL,
                 link.functions$alpha(th.alpha[1]),
                 link.functions$c_(th.c[1]),
                 link.functions$p(th.p[1]))
-    
+
     #cat('theta - LogL', theta_, '\n')
     comp. <- compute.grid(param. = theta_, list.input_ = list.input_)
     #print(sum(is.na(comp.list$It)))
@@ -86,14 +86,14 @@ temporal.ETAS <- function(sample.s, M0, T1, T2, link.functions = NULL,
     out <- theta_[3]*(list.input_$df_grid$magnitudes - list.input_$M0) + log(theta_[2] + 1e-100) + log(comp. + 1e-100)
     out
   }
-  
+
   # creating formula for past events contributions to integrated lambda
   # third is for the sum of the log intensities
   df.s <- data.frame(counts = nrow(sample.s), exposures = 0, part = 'SL')
-  
+
   ## MN: Function to calculate sum of log intensities using past events
   loglambda.inla <- function(th.mu, th.K, th.alpha, th.c, th.p, tt, th, mh, M0){
-    
+
     ## MN: Rescale parameters to internal parameter scale
     if(is.null(link.functions)){
       th.p <- c(th.mu[1], th.K[1], th.alpha[1], th.c[1], th.p[1])
@@ -105,7 +105,7 @@ temporal.ETAS <- function(sample.s, M0, T1, T2, link.functions = NULL,
                 link.functions$c_(th.c[1]),
                 link.functions$p(th.p[1]))
     }
-    
+
     ## MN: Parallel looping over the historic event magnitudes and times QQ why mean?  ALSO past in grid or historic?
     ## MN: Finn's trick for making more stable - QQ is this to avoid large numbers??
     mean(unlist(mclapply(tt, \(x) {
@@ -115,7 +115,7 @@ temporal.ETAS <- function(sample.s, M0, T1, T2, link.functions = NULL,
     },
     mc.cores = 5)))
   }
-  
+
   list.input <- list(df_grid = df.j, M0 = M0, Imapping = Imapping, time.sel = time.sel,
                      sample.s = sample.s)
   data.input = bind_rows(df.0, df.s, df.j)      ## Combine
@@ -125,7 +125,7 @@ temporal.ETAS <- function(sample.s, M0, T1, T2, link.functions = NULL,
                             idx.sl = data.input$part == 'SL'))
   predictor.fun <- function(th.mu, th.K, th.alpha, th.c, th.p,
                             list.input, T1, T2, M0){
-    
+
     out <- rep(0, nrow(data.input))
     out[list.input$idx.bkg] <- log(link.functions$mu(th.mu[1])) + log(T2 - T1)
     out[list.input$idx.trig] <- logLambda.h.inla(th.K = th.K, th.alpha = th.alpha,
@@ -139,7 +139,7 @@ temporal.ETAS <- function(sample.s, M0, T1, T2, link.functions = NULL,
                                              M0 = M0)
     out
   }
-  
+
   merged.form <- counts ~ predictor.fun(th.mu = th.mu, th.K = th.K, th.alpha = th.alpha,
                                         th.c = th.c, th.p = th.p, list.input = list.input,
                                         T1= T1, T2 = T2, M0 = M0)
@@ -149,10 +149,10 @@ temporal.ETAS <- function(sample.s, M0, T1, T2, link.functions = NULL,
     th.alpha(1, model = 'linear', mean.linear = 0, prec.linear = 1) +
     th.c(1, model = 'linear', mean.linear = 0, prec.linear = 1) +
     th.p(1, model = 'linear', mean.linear = 0, prec.linear = 1)
-  
+
   return( bru(formula = merged.form, components = cmp.part, data = data.input, family = 'Poisson',
               options = append(bru.opt, list(E = data.input$exposure))) )
-  
+
 }
 
 
