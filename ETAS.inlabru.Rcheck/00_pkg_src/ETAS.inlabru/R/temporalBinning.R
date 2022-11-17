f94509f32b8683f7f99a584500ba77ce3cb30547
mark-naylor <mark.naylor@ed.ac.uk>
2022-11-17 12:16:38 +0000

    Added a working notebook to demonstrate the effect of conditioning the model on the history

--- ETAS.inlabru.Rcheck/00_pkg_src/ETAS.inlabru/R/temporalBinning.R
+++ ETAS.inlabru.Rcheck/00_pkg_src/ETAS.inlabru/R/temporalBinning.R
@@ -1,91 +1,111 @@
-# find breaks point for grid
-#' Title
+
+#' Find breaks point for 1D grid
+#' @description `breaks_exp` return the breaks points of a one dimensional grid depending on three parameters, see details
+#' @param start.grid Starting point of the grid, `scalar`.
+#' @param end.grid End point of the grid, `scalar`.
+#' @param coef.t TimeBinning parameter: \eqn{\delta > 0} determines the relative length of subsequent intervals, `scalar`.
+#' @param delta.t TimeBinning parameter: \eqn{\Delta > 0} determines the length of the intervals, `scalar`.
+#' @param N.exp. TimeBinning parameter: \eqn{n_{max} >0} determines the maximum number of intervals, `scalar`
 #'
-#' @param tt_ List of the event times a grid is needed for [days].
-#' @param T2_ End of temporal domain [days].
-#' @param coef_ TimeBinning parameter:
-#' @param delta_ TimeBinning parameter:
-#' @param N_exp_ TimeBinning parameter: Number of bins
-#'
-#' @return
-#' @export
+#' @details The grid is calculated as follows
+#' \deqn{t, t + \Delta, t + \Delta(1 + \delta), t + \Delta(1 + \delta)^2,...., T}
+#' where \eqn{t} is the `start.grid` argument, \eqn{T} is the `end.grid` argument, and \eqn{n_{max}} is the maximum value of the exponent
+#' @return `vector` containing the grid points
 #'
 #' @examples
-breaks_exp <- function(tt_, T2_, coef_ = 2, delta_, N_exp_ = 10){
+#' breaks_exp(start.grid = 1, end.grid = 100, coef.t = 1, delta.t = 1, N.exp. = 3)
+#' breaks_exp(start.grid = 1, end.grid = 100, coef.t = 1, delta.t = 1, N.exp. = 10)
+breaks_exp <- function(start.grid, end.grid, coef.t = 2, delta.t, N.exp. = 10){
 
   # Generate the breaks for each event
-  tt_breaks <- tt_ + delta_*((1 + coef_)^(0:N_exp_))
+  tt_breaks <- start.grid + delta.t*((1 + coef.t)^(0:N.exp.))
 
   # Remove the breaks ending before the end of the temporal domain, T2.
-  tt_breaks <- tt_breaks[tt_breaks < T2_]
+  tt_breaks <- tt_breaks[tt_breaks < end.grid]
 
   # If domain smaller than min threshold, only return bounds
-  if(T2_ - tt_ < delta_){
-    return(c(tt_, T2_))
+  if(end.grid - start.grid < delta.t){
+    return(c(start.grid, end.grid))
   }
-  if(T2_ - tt_breaks[length(tt_breaks)] < delta_){
-    tt_breaks[length(tt_breaks)] = T2_
+  if(end.grid - tt_breaks[length(tt_breaks)] < delta.t){
+    tt_breaks[length(tt_breaks)] = end.grid
   }
-  if(tt_breaks[length(tt_breaks)] < T2_){
-    tt_breaks <- c(tt_breaks, T2_)
+  if(tt_breaks[length(tt_breaks)] < end.grid){
+    tt_breaks <- c(tt_breaks, end.grid)
   }
-  return(c(tt_,tt_breaks))
+  return(c(start.grid,tt_breaks))
 }
 
 
-#' Generate a set of time bins for a specific event and return.
+#' Generate a set of time bins for a specific event.
+#' @param data.point Point for which the binning is calculated, `list` with elements time (`ts, scalar`), event index (`idx.p, scalar`). Names are mandatory and should not be changed
+#' @param coef.t TimeBinning parameter: look [breaks_exp()]
+#' @param delta.t TimeBinning parameter: look [breaks_exp()]
+#' @param N.exp. TimeBinning parameter: look [breaks_exp()]
+#' @param T1. Start of the temporal domain, `scalar`
+#' @param T2. End of the temporal domain `scalar`.
 #'
-#' @param data.point
-#' @param coef.t TimeBinning parameter:
-#' @param delta.t TimeBinning parameter:
-#' @param T2. End of the temporal domain [days].
-#' @param displaygrid Boolean variable - whether to plot the grid
-#' @param N.exp. TimeBinning parameter:
-#'
-#' @return A set of time bins aggregated over all events MORE DETAIL
+#' @return
+#' A `data.frame` with as many rows as the number of bins and fixed number of columns.
+#' The columns are
+#' \itemize{
+#' \item `t.start` : starting point of the bin (minimum = `T1.`).
+#' \item `t.end` : end point of the bin. (maximum = `T2.`).
+#' \item `t.bin.name` : unique bin identifier.
+#' \item `t.ref_layer` : bin identifier for calculations
+#' \item `ts` : time provided in `data.point`
+#' \item `idx.p` : identifier provided in `data.point`
+#' }
+#' The bins are only between `T1.` and `T2.` or containing `T1.`
 #' @export
 #'
 #' @examples
 #' ## EXAMPLE 1
-#' events <- data.frame( ts=c(0,1 , 3 ), idx.p=c(1,2,3) )
-#' T2 <- 20
-#' N.exp <- 8
-#' delta.t <- 0.1
-#' coef.t <- 1
-#' time.grid(events, coef.t, delta.t, T2, displaygrid = FALSE, N.exp)
-time.grid <- function(data.point, coef.t, delta.t,
-                      T2., displaygrid = FALSE, N.exp.){
-
+#' event <- list( ts= 0, idx.p= 1 )
+#' time.grid(data.point = event, coef.t = 1, delta.t = 0.1, N.exp. = 8, T1 = 1, T2 = 20)
+time.grid <- function(data.point, coef.t, delta.t, N.exp.,
+                      T1., T2.){
+  # extract point information
   tt. <- data.point$ts
   idx.p <- data.point$idx.p
-  # spatial bins
-  if(displaygrid){
-    Plot_grid(xx = xx., yy = yy., delta_ = delta., n.layer = n.layer.,
-              bdy_ =  bdy., min.edge = min.edge.)
-  }
 
   # time bins
   # find bins break points
-  t_b <- breaks_exp(tt., T2., coef_ = coef.t, delta_ = delta.t, N_exp_ = N.exp.)
+  t_b <- breaks_exp(start.grid = tt.,
+                    end.grid = T2.,
+                    coef.t = coef.t,
+                    delta.t = delta.t,
+                    N.exp. = N.exp.)
+  # create data.frame with t.start (bin starting time), t.end (bin end time) and t.bin.name (unique name for each bin)
   time.bins <- data.frame(t.start = t_b[-length(t_b)],
                           t.end = t_b[-1]) %>%
     mutate(t.bin.name = paste0(round(t.start,3),'-',round(t.end,3)))
+  # if there is only 1 bin than it is the last
   if(nrow(time.bins) - 1 == 0){
     time.bins$t.ref_layer = paste0('last-',idx.p)
   }
+  # assign a number to each bin except the last one.
   else{
     time.bins$t.ref_layer = c(1:(nrow(time.bins) - 1), paste0('last-',idx.p))
   }
-  return( cbind(time.bins, data.point, row.names = NULL) )
+  # remove bins before T1
+  bin.before <- time.bins$t.end < T1.
+  time.bins <- time.bins[!bin.before,]
+  # change name to bins containing T1
+  if(sum(time.bins$t.start <= T1. & time.bins$t.end >= T1.) > 0){
+    time.bins$t.ref_layer[time.bins$t.start <= T1. & time.bins$t.end >= T1.] <- paste0('between-',idx.p)
+    time.bins$t.start[time.bins$t.start <= T1. & time.bins$t.end >= T1.] <- T1.
+  }
+  # merge with data.frame taken in input
+  cbind(time.bins, data.point, row.names = NULL)
 }
 
-#' Title
+#' Function to calculate the integral of the Omori's law
 #'
-#' @param param_
-#' @param time.df
+#' @param param_ ETAS parameters vector (\eqn{\mu, K, \alpha, c, p}), only \eqn{c, p} are used.
+#' @param time.df output of the function [time.grid()]
 #'
-#' @return
-#' @export
+#' @return `vector` of integral values between each bin provided in `time.df`
 #'
 #' @examples
 It_df <- function(param_, time.df){
@@ -94,20 +114,23 @@ It_df <- function(param_, time.df){
   T2b <- as.numeric(time.df$t.end)
   param_c <- param_[4]
   param_p <- param_[5]
-  T.l <- pmax(tth, T1b) #sapply(1:length(tth), \(x) max(tth[x], T1b[x]))
+  T.l <- pmax(tth, T1b)
   fun.l <- (1 + (T.l - tth)/param_c)^(1-param_p)
   fun.u <- (1 + (T2b - tth)/param_c)^(1-param_p)
   ( param_c/ (param_p - 1) )* ( fun.l - fun.u )
 }
 
 
-#' Title
+#' Function to compute the integral of Omori's law efficiently
 #'
-#' @param param.
-#' @param list.input_
-#'
-#' @return
-#' @export
+#' @param param. ETAS parameters vector (\eqn{\mu, K, \alpha, c, p}), only \eqn{c, p} are used.
+#' @param list.input_ `list` containing information to calculate the integrals efficiently. The list is created inside the `Temporal.ETAS` function
+#' Two elements are used
+#' \itemize{
+#' \item `time.sel` selection of rows of the output of `time.grid` with unique `t.ref_layer` value, `data.frame`.
+#' \item `Imapping` mapper between the unique names provided in `time.sel` and original rows of the output of `time.grid()`, `vector`.
+#' }
+#' @return `vector` with same length as `list.input_$Imapping` with the integral of the Omori's law for each bin.
 #'
 #' @examples
 compute.grid <- function(param., list.input_){
--- ETAS.inlabru.Rcheck/ETAS.inlabru-Ex.R
+++ ETAS.inlabru.Rcheck/ETAS.inlabru-Ex.R
@@ -1,16 +1,49 @@
 pkgname <- "ETAS.inlabru"
 source(file.path(R.home("share"), "R", "examples-header.R"))
 options(warn = 1)
+base::assign(".ExTimings", "ETAS.inlabru-Ex.timings", pos = 'CheckExEnv')
+base::cat("name\tuser\tsystem\telapsed\n", file=base::get(".ExTimings", pos = 'CheckExEnv'))
+base::assign(".format_ptime",
+function(x) {
+  if(!is.na(x[4L])) x[1L] <- x[1L] + x[4L]
+  if(!is.na(x[5L])) x[2L] <- x[2L] + x[5L]
+  options(OutDec = '.')
+  format(x[1L:3L], digits = 7L)
+},
+pos = 'CheckExEnv')
+
+### * </HEADER>
 library('ETAS.inlabru')
 
 base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
 base::assign(".old_wd", base::getwd(), pos = 'CheckExEnv')
+cleanEx()
+nameEx("breaks_exp")
+### * breaks_exp
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: breaks_exp
+### Title: Find breaks point for 1D grid
+### Aliases: breaks_exp
+
+### ** Examples
+
+breaks_exp(start.grid = 1, end.grid = 100, coef.t = 1, delta.t = 1, N.exp. = 3)
+breaks_exp(start.grid = 1, end.grid = 100, coef.t = 1, delta.t = 1, N.exp. = 10)
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("breaks_exp", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("create.input.list.temporal.noCatalogue")
 ### * create.input.list.temporal.noCatalogue
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: create.input.list.temporal.noCatalogue
 ### Title: Function to create a default input file for the ETAS Hawkes
 ###   temporal model where no catalogue is specified in the input file
@@ -23,12 +56,15 @@ flush(stderr()); flush(stdout())
 
 
 
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("create.input.list.temporal.noCatalogue", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("generate.temporal.ETAS.synthetic")
 ### * generate.temporal.ETAS.synthetic
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: generate.temporal.ETAS.synthetic
 ### Title: Generates a sythetic catalogue using the ETAS model
 ### Aliases: generate.temporal.ETAS.synthetic
@@ -47,12 +83,15 @@ generate.temporal.ETAS.synthetic( theta=data.frame(mu=0.1, K=0.089, alpha=2.29, 
 
 
 
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("generate.temporal.ETAS.synthetic", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("sample.GR.magnitudes")
 ### * sample.GR.magnitudes
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: sample.GR.magnitudes
 ### Title: Return a sample of magnitudes drawn from the GR distribution
 ### Aliases: sample.GR.magnitudes
@@ -63,12 +102,15 @@ sample.GR.magnitudes(n=100, beta.p=log(10), M0=2.5)
 
 
 
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("sample.GR.magnitudes", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("sample.temporal.ETAS.generation")
 ### * sample.temporal.ETAS.generation
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: sample.temporal.ETAS.generation
 ### Title: Take all previous parent events from 'Ht=data.frame[ts,
 ###   magnitudes]' and generates their daughters events using the ETAS
@@ -83,28 +125,29 @@ sample.temporal.ETAS.generation( theta=data.frame(mu=0.1, K=0.089, alpha=2.29, c
 
 
 
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("sample.temporal.ETAS.generation", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("time.grid")
 ### * time.grid
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: time.grid
-### Title: Generate a set of time bins for a specific event and return.
+### Title: Generate a set of time bins for a specific event.
 ### Aliases: time.grid
 
 ### ** Examples
 
 ## EXAMPLE 1
-events <- data.frame( ts=c(0,1 , 3 ), idx.p=c(1,2,3) )
-T2 <- 20
-N.exp <- 8
-delta.t <- 0.1
-coef.t <- 1
-time.grid(events, coef.t, delta.t, T2, displaygrid = FALSE, N.exp)
+event <- list( ts= 0, idx.p= 1 )
+time.grid(data.point = event, coef.t = 1, delta.t = 0.1, N.exp. = 8, T1 = 1, T2 = 20)
 
 
 
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("time.grid", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 ### * <FOOTER>
 ###
 cleanEx()
