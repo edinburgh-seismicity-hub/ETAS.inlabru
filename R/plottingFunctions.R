d26a4bd4071085395333a60be476e9386ae1a533
Serra314 <serafini314@gmail.com>
2023-06-28 15:07:41 +0100

    Modified omori_plot_posterior to take as input the posterior samples.

--- R/plottingFunctions.R
+++ R/plottingFunctions.R
@@ -252,6 +252,8 @@ omori_plot_prior <- function(input.list, n.samp = 10, t.end = 1, n.breaks = 100)
 #' \item `model.fit`: `bru` object used to sample the posterior of the ETAS parameters
 #' \item `link.functions`: `list` of functions to convert the ETAS parameters from the INLA scale to the ETAS scale
 #' }
+#' #' @param post.samp `data.frame` containing posterior samples of the parameters. If `NULL`, then `n.samp` samples are
+#' generated. If `n.samp` is different from `nrow(post.samp)` then `n.samp` rows are uniformly sampled from `post.samp`. Default is `NULL`.
 #' @param n.samp Number of posterior samples, `integer` (`default = 10`).
 #' @param t.end Upper bound of the x-axis, `scalar` (`default = 1`).
 #' @param n.breaks Number of points between 0 and `t.end` to calculate the function, `integer` (`default = 100`).
@@ -259,20 +261,27 @@ omori_plot_prior <- function(input.list, n.samp = 10, t.end = 1, n.breaks = 100)
 #' @return A ggplot object
 #' @seealso [create.input.list.temporal.noCatalogue()], [create.input.list.temporal.withCatalogue()]
 #' @export
-omori_plot_posterior <- function(input.list, n.samp = 10, t.end = 1, n.breaks = 100){
+omori_plot_posterior <- function(input.list, post.samp = NULL, n.samp = 10, t.end = 1, n.breaks = 100){
   t.eval <- seq(1e-6, t.end, length.out = n.breaks)
-  post.samp <- inlabru::generate(
-    input.list$model.fit,
-    data.frame(),
-    ~ c(input.list$link.functions$mu(th.mu),
-        input.list$link.functions$K(th.K),
-        input.list$link.functions$alpha(th.alpha),
-        input.list$link.functions$c_(th.c),
-        input.list$link.functions$p(th.p)
-    ),
-    n.samples = n.samp)
-  post.samp <- t(post.samp)
-
+  if(is.null(post.samp)){
+    post.samp <- inlabru::generate(
+      input.list$model.fit,
+      data.frame(),
+      ~ c(input.list$link.functions$mu(th.mu),
+          input.list$link.functions$K(th.K),
+          input.list$link.functions$alpha(th.alpha),
+          input.list$link.functions$c_(th.c),
+          input.list$link.functions$p(th.p)
+      ),
+      n.samples = n.samp)
+    post.samp <- t(post.samp)
+  }
+  if((!is.null(post.samp))&(!is.null(n.samp))){
+    if(nrow(post.samp) != n.samp){
+      idx.samp <- sample(1:nrow(post.samp), n.samp, replace = TRUE)
+      post.samp <- post.samp[idx.samp,]
+    }
+  }
   omori.eval <- lapply(seq_len(nrow(post.samp)),
                        \(x) omori(theta = post.samp[x,],
                                   t = t.eval,
